980909b314da2369078696d0ccaea9bf2da901d2
mbeznogov <57128711+mbeznogov@users.noreply.github.com>
2023-04-24 11:32:03 +0300

    переделал ещё раз
    ссылки ток кривые, но главное чтоб запустилось - у себя не тестил

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
-import telebot
 
+import telebot
+from telebot import types
+
+# Создаем бота
 BOT_TOKEN = ('5773482626:AAGfWoGsyqVvqM1ky-HLaYXod6v2pSvBgfA')
 bot = telebot.TeleBot(BOT_TOKEN)
-keyboard1 = telebot.types.ReplyKeyboardMarkup()
-keyboard1 = telebot.types.ReplyKeyboardMarkup(True)
-keyboard1.row('Информатика', 'Математика', 'Русский язык', 'Английский')
-
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.from_user.id, 'Привет,какие задачи вас интересуют?', reply_markup=keyboard1)
-
-@bot.message_handler(content_types=['text'])
-def send_text(message):
-    if message.text.lower() == 'Информатика':
-        bot.send_message(message.from_user.id, 'ссылка')
-
-    elif message.text.lower() == 'Математика':
-        bot.send_message(message.from_user.id, 'Numble: https://numble.wtf/')
-        bot.send_message(message.from_user.id, 'Cogit: https://cogit.fun/play')
-        bot.send_message(message.from_user.id, 'Angle: https://angle.wtf/', reply_markup=keyboard1)
-
-    elif message.text.lower() == 'Русский язык':
-        bot.send_message(message.from_user.id, 'wordle: https://wordle.belousov.one/', reply_markup=keyboard1)
-
-    elif message.text.lower() == 'Английский':
-        bot.send_message(message.from_user.id, 'squardle: https://fubargames.se/squardle/')
-        bot.send_message(message.from_user.id, 'CONTEXTO: https://contexto.me/', reply_markup=keyboard1)
-
-    elif message.text.lower() == 'География':
-        bot.send_message(message.from_user.id, 'globle: https://globle-game.com/')
-        bot.send_message(message.from_user.id, 'WORLDLE: https://worldle.teuteuf.fr/')
-        bot.send_message(message.from_user.id, 'WORLDLE: https://worldle.teuteuf.fr/', reply_markup=keyboard1)
-
-
+# Команда start
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+        # Добавляем кнопки
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1=types.KeyboardButton("Информатика")
+        item2=types.KeyboardButton("Математика")
+        item3=types.KeyboardButton("Русский")
+        item4=types.KeyboardButton("Английский")
+        item4=types.KeyboardButton("География")
+        markup.add(item1)
+        markup.add(item2)
+        markup.add(item3)
+        markup.add(item4)
+        markup.add(item5)
+        bot.send_message(m.chat.id, 'Привет, какие задачи вас интересуют?',  reply_markup=markup)
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    # Если юзер прислал 1
+    if message.text.strip() == 'Информатика':
+            answer = 'Numble: https://numble.wtf/'
+    # Если юзер прислал 2
+    elif message.text.strip() == 'Математика':
+            answer = 'wordle: https://wordle.belousov.one/'
+    # Если юзер прислал 3
+    elif message.text.strip() == 'Русский':
+            answer = 'squardle: https://fubargames.se/squardle/'
+    # Если юзер прислал 4
+    elif message.text.strip() == 'Английский':
+            answer = 'globle: https://globle-game.com/'
+    # Если юзер прислал 5
+    elif message.text.strip() == 'География':
+            answer = 'WORLDLE: https://worldle.teuteuf.fr/'
+    # Отсылаем юзеру сообщение в его чат
+    bot.send_message(message.chat.id, answer)
+# Запускаем бота
 bot.polling(none_stop=True, interval=0)
